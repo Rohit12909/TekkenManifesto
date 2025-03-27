24459bec74e17c0c450b57f4345cec3b4f943330
Rohit12909 <Rohit12909@gmail.com>
2025-03-27 15:28:46 -0400

    Added Service and Repository layers for new models

--- tekkenManifesto.API/4_Repository/Inteface/IRepository.cs
+++ tekkenManifesto.API/4_Repository/Inteface/IRepository.cs
@@ -14,5 +14,40 @@ public interface ICharRepository
 
 public interface IComboRepository
 {
-    
+    Combo? CreateNewCombo(Combo c);
+    IEnumerable<Combo> GetAllCombos();
+    Combo UpdateCombo(Combo update, int id);
+    Combo? DeleteCombo(int id);
+}
+
+public interface IHERepository
+{
+    HeatEngager? CreateNewHE(HeatEngager c);
+    IEnumerable<HeatEngager> GetAllHE();
+    HeatEngager UpdateHE(HeatEngager update, int id);
+    HeatEngager? DeleteHE(int id);
+}
+
+public interface ILauncherRepository
+{
+    Launcher? CreateLauncher(Launcher c);
+    IEnumerable<Launcher> GetAllLaunchers();
+    Launcher UpdateLauncher(Launcher update, int id);
+    Launcher? DeleteLauncher(int id);
+}
+
+public interface IPunisherRepository
+{
+    Punisher? CreatePunisher(Punisher c);
+    IEnumerable<Punisher> GetAllPunishers();
+    Punisher UpdatePunisher(Punisher update, int id);
+    Punisher? DeletePunisher(int id);
+}
+
+public interface IStanceRepository
+{
+    Stance? CreateStance(Stance c);
+    IEnumerable<Stance> GetAllStances();
+    Stance UpdateStance(Stance update, int id);
+    Stance? DeleteStance(int id);
 }
