9a9c225b83ea033365b6079024ea1a4af4b55e1b
Rohit12909 <Rohit12909@gmail.com>
2025-03-27 16:10:39 -0400

    Implemented Heat Engager Service

--- tekkenManifesto.API/1_Model/HeatEngager.cs
+++ tekkenManifesto.API/1_Model/HeatEngager.cs
@@ -1,7 +1,10 @@
+using System.Text.Json.Serialization;
+
 namespace tekkenManifesto.API.Model;
 
 public class HeatEngager
 {
+    [JsonIgnore]
     public int Id { get; set; }
     public string Input { get; set; } = "";
     public string FramesOB { get; set; } = "";
---
+++ tekkenManifesto.API/3_Service/HEService.cs
@@ -0,0 +1,50 @@
+using tekkenManifesto.API.Model;
+using tekkenManifesto.API.Repository;
+
+namespace tekkenManifesto.API.Service;
+
+public class HeatEngagerService : IHEService
+{
+    private readonly IHERepository _heRepository;
+
+    public HeatEngagerService(IHERepository heRepository)
+    {
+        _heRepository = heRepository;
+    }
+
+    public HeatEngager? CreateNewHE(HeatEngager c)
+    {
+        return _heRepository.CreateNewHE(c);
+    }
+
+    public HeatEngager? DeleteHE(int id)
+    {
+        var deleted = _heRepository.DeleteHE(_heRepository.GetHEById(id));
+        return deleted;
+    }
+
+    public IEnumerable<HeatEngager> GetAllHE()
+    {
+        var heList = _heRepository.GetAllHE();
+        return heList;
+    }
+
+    public HeatEngager UpdateHE(HeatEngager update, int id)
+    {
+         var updated = _heRepository.GetHEById(id);
+
+        if (!string.IsNullOrWhiteSpace(update.Input))
+        {
+            updated.Input = update.Input;
+        }
+
+        if (!string.IsNullOrWhiteSpace(update.FramesOB))
+        {
+            updated.FramesOB = update.FramesOB;
+        }
+
+        updated = _heRepository.UpdateHE(updated);
+
+        return updated;
+    }
+}
